ae94554b15eac4f40ff4a369580a3382c50a47e6
iDoesNotCompute <luke@badfngamers.com>
2020-11-29 13:41:17 -0700

    Bug fixes

--- updater.sh
+++ updater.sh
@@ -33,31 +33,19 @@ do
   basnamefile=$(basename "${f}")
   tempfile="${basnamefile}.temp"
 
-  printf "\nPreparing to download ${url}${basnamefile} into ${basepath}${tempfile}\n"
-  printf "\nFile: ${f}\n"
-
   #Download plugin to temp file. Umod web server does not have timestamping turned on so Last-Modified header is not available
-  #if [curl --fail --fail-early -o ${basepath}${tempfile} ${url}${basnamefile} 1>> updater.log 2> /dev/null]; then
-    #printf "$(date +%f_%T) - File download failed with error $?, exiting. Check logs\n" |& tee -a updater.log
-    #exit 1
-  #fi
   curl --fail --fail-early -o ${basepath}${tempfile} ${url}${basnamefile}
 
-  stat ${basepath}${tempfile}
-
-  cat ${basepath}${tempfile} | echo
-
-  #Check differences in downloaded plugin to plugin on disk
+  # Check differences in downloaded plugin to plugin on disk
   diff -s ${basepath}${tempfile} ${f} 1>> updater.log  2> /dev/null
 
   if [[ $? == "1" ]]; then
     ((updated++))
     printf "Change detected, updating plugin ${basnamefile}.\n" &>> updater.log
-    # Copy temp file of updated plugin
 
-    #if [mv "${tempfile}" "${f}" &>> updater.log]; then
-    if [yes | cp -f "${basepath}${tempfile}" "${f}" &>> updater.log]; then
-      printf "$(date +%f_%T) - File copy failed, exiting. Check logs\n" |& tee -a updater.log
+    # Copy temp file of updated plugin
+    if [mv "${basepath}${tempfile}" "${f}" &>> updater.log]; then
+      printf "\n$(date +%f_%T) - File copy failed, exiting. Check logs\n" |& tee -a updater.log
       exit 1
     fi
   else
@@ -71,4 +59,4 @@ do
   sleep 0.5 #Sleep to not spam the web server
 done
 
-printf "$(date +%f_%T) - Finished! Updated $updated plugins. Check updater.log for details.\n" |& tee -a updater.log
+printf "\n$(date +%f_%T) - Finished! Updated $updated plugins. Check updater.log for details.\n" |& tee -a updater.log
